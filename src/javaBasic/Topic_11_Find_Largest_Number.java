61e31ff4de08a4472f6c48d6b7b0b86413f7abc8
Phạm Phan Nhật Khánh <phamphannhatkhanh7520@gmail.com>
2025-03-05 14:25:19 +0700

    Learning Java Basics

---
+++ src/javaBasic/Topic_11_Find_Largest_Number.java
@@ -0,0 +1,47 @@
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Test;
+
+
+// Tìm số lớn nhất trong mảng
+public class Topic_11_Find_Largest_Number {
+
+    int number [] = {5, 8, 15, 7, 60, 10};
+
+    @Test
+    public void TC_01_Find_Max_Number(){
+        int x = 0;
+        for (int i = 0; i < number.length; i++) {
+            if(x < number[i]) {
+                x = number[i];
+            }
+        }
+
+        System.out.println("First number = " + number[0]);
+        System.out.println("Last number = " + number[number.length-1]);
+        System.out.println("Max number = " + x);
+    }
+
+    @Test
+    @Description("")
+    public void TC_02_calculate_Sum_First_And_Last_Number() {
+        int x = 0;
+        x = number[0] + number[number.length-1];
+        System.out.println("Sum : " + x);
+
+    }
+
+    @Test
+    @Description("")
+    public void TC_05_Calculate_Sum_Average(){
+        int sum = 0;
+        for (int i = 0; i < number.length; i++) {
+            sum += number[i];
+        }
+
+        System.out.println("Sum all number = " + sum);
+
+        float averageNumber = sum / number.length;
+        System.out.println("Average all number = " +averageNumber);
+    }
+
+}
---
+++ src/javaBasic/Topic_12_String.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.util.Arrays;
+
+public class Topic_12_String {
+
+    public static void main(String[] args) {
+        String schoolName = "Automation Testing Advanced";
+        String schoolAddress = "Ho Chi Minh city";
+        System.out.println(schoolName.length());
+
+        System.out.println("Vị trí của 1 ký tự/ chuỗi kí tự trong chuỗi = " + schoolName.indexOf("Automation"));
+        System.out.println("Vị trí của 1 ký tự/ chuỗi kí tự trong chuỗi = " + schoolName.indexOf("T"));
+
+        System.out.println("----------------------------------------------------------------------");
+
+        // Substring
+        System.out.println("Tách chuỗi : " + schoolName.substring(11));
+        System.out.println("Tách chuỗi : " + schoolName.substring(11,15));
+
+        String result = "Viewing 48 of 132 results";
+        String results[] = result.split(" "); // Tách chuỗi thành 1 mảng dựa vào kí tự / chuỗi kí tự
+        System.out.println(results[1]);
+
+        System.out.println("----------------------------------------------------------------------");
+
+        // Replace
+        String productPrice = "$100.00";
+        productPrice= productPrice.replace("$","");
+        System.out.println("Before converting into float : " + productPrice);
+
+        // Sap xếp nó: Sort Data (Asc/Desc)
+        float productPriceF = Float.parseFloat(productPrice);
+        System.out.println(productPriceF);
+
+        System.out.println("----------------------------------------------------------------------");
+
+        String driver = "libs/chromedriver";
+        String osName = System.getProperty("os.name");
+        System.out.println("Your os name : " + osName);
+
+        // Handle operation model
+        if(osName.toLowerCase().contains("windows")){
+            Keys key = Keys.CONTROL;
+        } else {
+            Keys key = Keys.COMMAND;
+        }
+
+        String driverInstanceName = driver.toString();
+        System.out.println("Driver Instance Name : " + driverInstanceName);
+
+    }
+
+    @Test
+    public void splitString(){
+        String text = "AirAsia Vietnam";
+        String splitedString = Arrays.toString(text.split(" "));
+        System.out.println("text : " + splitedString );
+    }
+
+
+
+
+}
